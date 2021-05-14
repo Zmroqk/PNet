ab7a7bdd65c1476e54c9db86b589d747afcb9ed8
Adrian Żak <adrian.damian.zak@gmail.com>
2021-05-14 18:09:13 +0200

    Added ping reply.

---
+++ PingData.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace PNet
+{
+    public class PingData
+    {
+        public long ActualPing { get; set; }
+
+        public long AveragePing { get; set; }
+
+        public long MaxPing { get; set; }
+
+        public int PacketsSend { get; set; }
+
+        public int PacketsReceived { get; set; }
+
+        public float PacketLoss { get; set; }
+
+        public string Hostname { get; set; }
+
+
+        IPAddress iPAddress;
+
+        int timeout;
+
+        List<long> lastIps = new List<long>();
+
+        int connectionErrors = 0;
+
+        bool hostnameResolved = false;
+
+        public PingData(IPAddress ip, int timeout = 5000)
+        {
+            this.iPAddress = ip;
+            this.timeout = timeout;
+        }
+
+        public Task PingAsync()
+        {
+            if (hostnameResolved)
+            {
+                Hostname = Dns.GetHostEntry(iPAddress).HostName;
+                hostnameResolved = true;
+            }
+            Ping ping = new Ping();
+            PacketsSend++;
+            return ping.SendPingAsync(iPAddress, timeout).ContinueWith(PingCompleted);
+        }
+
+        void PingCompleted(Task<PingReply> pr)
+        {
+            PingReply pingReply = pr.Result;
+            if(pingReply.Status == IPStatus.Success)
+            {
+                PacketsReceived++;
+                ActualPing = pingReply.RoundtripTime;
+                if (pingReply.RoundtripTime < MaxPing)
+                    MaxPing = pingReply.RoundtripTime;
+            }
+        }
+    }
+}
