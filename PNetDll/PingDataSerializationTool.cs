3ef268a035a3cb92c24f898d7a4c9775a0a79137
Adrian Żak <48351330+Zmroqk@users.noreply.github.com>
2021-05-18 14:15:12 +0200

    Fixes for data serialization/deserialization. (#9)
    Co-authored-by: Adrian Żak <adrian.damian.zak@gmail.com>

--- PNetDll/PingDataSerializationTool.cs
+++ PNetDll/PingDataSerializationTool.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Text.Json;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace PNetDll
@@ -25,14 +26,39 @@ namespace PNetDll
         }
 
         /// <summary>
+        /// Serialize ping data to stream
+        /// </summary>
+        /// <param name="stream">Stream to which ping data should be serialized</param>
+        /// <param name="pingData">Ping data </param>
+        /// <returns></returns>
+        public static Task SerializeAsync(Stream stream, PingData pingData)
+        {
+            return Task.Run(() =>
+            {
+                StreamWriter sw = new StreamWriter(stream);
+                sw.WriteLine(JsonSerializer.Serialize(pingData));
+            });            
+        }
+
+        /// <summary>
         /// Deserialize ping data from stream
         /// </summary>
         /// <param name="stream">Stream from which to read data</param>
         /// <returns>Ping data read from stream</returns>
-        public static PingData Deserialize(Stream stream)
+        public static Task<PingData> DeserializeAsync(Stream stream, CancellationTokenSource cancellationToken = null)
         {
-            StreamReader sr = new StreamReader(stream);
-            return JsonSerializer.Deserialize<PingData>(sr.ReadLine());
+            CancellationToken token = new CancellationToken();
+            if (cancellationToken != null)
+                token = cancellationToken.Token;
+            return Task.Run(() => {
+                StreamReader sr = new StreamReader(stream);
+                string line;
+                while ((line = sr.ReadLine()) == null){
+                    Task.Delay(1000);
+                    token.ThrowIfCancellationRequested();
+                }
+                return JsonSerializer.Deserialize<PingData>(line);
+            }, token);
         }
     }
 }
